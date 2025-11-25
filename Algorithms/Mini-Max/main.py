fbeb8b90438c0da81ef7264d8151a4987a444418
Ricardo Chávez <gic.rc455@gmail.com>
2025-11-24 23:14:10 -0600

    Update root directories

--- Algorithms/Mini-Max/main.py
+++ Algorithms/Mini-Max/main.py
@@ -1,23 +1,42 @@
 def main():
-    
+
     "Solucion Clasica del ejercio, similar a c y java"
     arr = [1, 2, 3, 4, 5]
-    large = len(arr)  # Solo es para recordar que se puede saber el largo mediante el metodo len() , devuelve un objeto completamente nuevo en realidad. 
-    
-    
+    large = len(arr)  # Solo es para recordar que se puede saber el largo mediante el metodo len() , devuelve un objeto completamente nuevo en realidad.
+
+
     # Inicializamos las variables o los acumuladores en 0
     suma_min = 0
     suma_max = 0
-    
+
     # Creamos un for sencillo que en vez de recorrer el arr simplemente, lo que hacemo es pasarle el objeto iterable range() para generar la sencuencia en i con: 0, 1, 2, 3
     for i in range(4):
         suma_min = suma_min + arr[i] # suma min guardamos en cada iteracion el arr con la posicon 0, 1, 2 ,3 y sus elementos contenidos y asi en cada iteración
     print(f"suma min es: {suma_min}") # Saliendo imprimimos en patanlla la suma de los primeros 4 numeros
-    
+
     # Creamos otro for para iterar sobre sobre los ultimos 4 numeros
     for i in range(1, 5):
         suma_max = suma_max + arr[i]
     print(f"suma max es: {suma_max}")
+
+def arr_len():
+    """
+    Solución emergente, mas verbosa
+    """
+
+    arr_len = [1, 2, 3, 4, 5]
+
+    longitud_arr = len(arr_len)
+    suma_min = 0
+    suma_max = 0
+    total = 0
+
+    for i in range(longitud_arr):
+        if i < 4 :
+            total = total + arr_len[i]
+
+    print(total)
+
 
 
 def minMaxSum():
@@ -34,10 +53,28 @@ def minMaxSum():
     sumMax = max(arr_minMaxSum)
 
     total = sum(arr_minMaxSum)
-  
+
     print(f"el {total - sumMin} {total - sumMax}")
+
+def solution_hackerrank():
+    
+    arr = [1,2,3,4,5]
+    sumMin = 0
+    sumMax = 0
+
+    arr_ordenado = sorted(arr)
+
+    for i in range(4):
+        sumMin = sumMin + arr_ordenado[i]
+
+    for j in range(1,5):
+        sumMax = sumMax + arr_ordenado[j]
+
+    print(f"{sumMin} {sumMax}")
+
 
 
 if __name__ == "__main__":
     main()
     minMaxSum()
+    arr_len()
